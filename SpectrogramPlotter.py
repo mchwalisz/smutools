2263af0d366d34bd175e337f7eef4021b87d8380
Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2013-02-27 17:29:04 +0100

    change to matplotlib figures
    - spectrogram works

--- SpectrogramPlotter.py
+++ SpectrogramPlotter.py
@@ -8,61 +8,52 @@ __copyright__ = "Copyright (c) 2013, Technische Universität Berlin"
 __version__ = "1.0.0"
 __email__ = "chwalisz@tkn.tu-berlin.de"
 
-from PyQt4 import Qt
-import PyQt4.Qwt5 as Qwt
-from PyQt4.Qwt5.anynumpy import *
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.cm as cm
+import logging
+import time
 
 
-class SpectrogramData(Qwt.QwtRasterData):
-    """docstring for SpectrogramData"""
-    def __init__(self):
-        Qwt.QwtRasterData.__init__(self, Qt.QRectF(-1.5, -1.5, 3.0, 3.0))
-
-    def copy(self):
-        return self
-
-    def range(self):
-        return Qwt.QwtDoubleInterval(0.0, 10.0)
-
-    def value(self, x, y):
-        c = 0.842
-        v1 = x * x + (y - c) * (y + c)
-        v2 = x * (y + c) + x * (y + c)
-        return 1.0 / (v1 * v1 + v2 * v2)
-
-
-class SpectrogramPlotter(Qwt.QwtPlot):
+class SpectrogramPlotter(FigureCanvas):
     """docstring for SpectrogramPlotter"""
     def __init__(self, *args):
-        Qwt.QwtPlot.__init__(self, *args)
-        self.spectrogram = Qwt.QwtPlotSpectrogram()
-        self.spectrogram.attach(self)
-
-        colorMap = Qwt.QwtLinearColorMap(Qt.Qt.darkCyan, Qt.Qt.red)
-        colorMap.addColorStop(0.1, Qt.Qt.cyan)
-        colorMap.addColorStop(0.6, Qt.Qt.green)
-        colorMap.addColorStop(0.95, Qt.Qt.yellow)
-
-        self.spectrogram.setColorMap(colorMap)
-
-        self.spectrogram.setData(SpectrogramData())
-
-        self.plotLayout().setAlignCanvasToScales(True)
-
-        self.setTitle("Spectrogram")
+        self.figure = Figure()
+        FigureCanvas.__init__(self, self.figure)
+        self.axes = self.figure.add_subplot(111)
+        self.log = logging.getLogger("measurement.SpectrogramPlotter")
 
     # __init__()
 
+    dBmMin = float("Inf")
+    dBmMax = -float("Inf")
+    image = None
+
     def updatePlot(self, fReader):
         if fReader.sweepCurrent is None:
-            print("Missing value")
+            self.log.warning("Missing data to plot")
             return
         if fReader.frequencyList is None:
+            self.log.warning("Missing frequency list")
             freqList = range(fReader.sweepCurrent.shape[0])
         else:
             freqList = fReader.frequencyList
-        # Update scale
+        self.dBmMin = min(np.nanmin(fReader.sweepMin), self.dBmMin)
+        self.dBmMax = max(np.nanmax(fReader.sweepMax), self.dBmMax)
+        if self.image is None:
+            self.image = self.axes.matshow(fReader.sweepAll, cmap=cm.spectral,
+                                           vmax=self.dBmMax, vmin=self.dBmMin,
+                                           animated=True, aspect="auto",
+                                           extent=[freqList[0], freqList[-1], 0, 10])
+        else:
+            self.image.set_data(fReader.sweepAll)
+            t0 = time.time() - (fReader.timeStart + fReader.timeStamp[-1])
+            t1 = time.time() - (fReader.timeStart + fReader.timeStamp[0])
+            self.image.set_extent([freqList[0], freqList[-1], t0, t1])
+        self.figure.canvas.draw()
 
-        self.replot()
+    # def updatePlot
+
 
 # class SpectrogramPlotter
