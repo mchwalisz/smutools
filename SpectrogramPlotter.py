e7e8e5fd58e7faf90d72a1388ebfb07609b33a41
Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2013-03-01 16:27:11 +0100

    calculate time for every plot

--- SpectrogramPlotter.py
+++ SpectrogramPlotter.py
@@ -41,15 +41,16 @@ class SpectrogramPlotter(FigureCanvas):
             freqList = fReader.frequencyList
         self.dBmMin = min(np.nanmin(fReader.sweepMin), self.dBmMin)
         self.dBmMax = max(np.nanmax(fReader.sweepMax), self.dBmMax)
+        t0 = time.time() - (fReader.timeStart + fReader.timeStamp[-1])
+        t1 = time.time() - (fReader.timeStart + fReader.timeStamp[0])
         if self.image is None:
             self.image = self.axes.matshow(fReader.sweepAll, cmap=cm.spectral,
                                            vmax=self.dBmMax, vmin=self.dBmMin,
                                            animated=True, aspect="auto",
-                                           extent=[freqList[0], freqList[-1], 0, 10])
+                                           extent=[freqList[0], freqList[-1], t0, t1])
         else:
             self.image.set_data(fReader.sweepAll)
-            t0 = time.time() - (fReader.timeStart + fReader.timeStamp[-1])
-            t1 = time.time() - (fReader.timeStart + fReader.timeStamp[0])
+
             self.image.set_extent([freqList[0], freqList[-1], t0, t1])
         self.figure.canvas.draw()
 
