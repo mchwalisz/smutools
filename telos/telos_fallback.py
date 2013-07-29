56f61addb2eba76e7017823a02a2ebbd61da9f91
Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2013-07-29 09:34:44 +0200

    fallback warning

--- telos/telos_fallback.py
+++ telos/telos_fallback.py
@@ -5,9 +5,14 @@ Created on 23-02-2012
 @author: Mikolaj Chwalisz
 '''
 import threading
+import logging
+
 
 class sensing(threading.Thread):
-	sema_install = threading.Semaphore()
-	
+    sema_install = threading.Semaphore()
+
+
 def list_devs():
-	return []
+    logger = logging.getLogger('sensing.telos')
+    logger.warning("Using telos_fallback module, no telos nodes will be detected")
+    return []
