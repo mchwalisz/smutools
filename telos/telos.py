667bae81a38059c276ec775c20d5b0a4008d18b1
Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2013-07-29 09:18:41 +0200

    fix PEP errors

--- telos/telos.py
+++ telos/telos.py
@@ -5,39 +5,42 @@ Created on 10-02-2012
 @author: Mikolaj Chwalisz
 '''
 import threading
-import subprocess, os
-import tempfile, shutil
+import subprocess
+import os
+import tempfile
+import shutil
 import logging
 
-sensing_dir = '../telos_spectrum_monitor/' # Has to relative to the the telos.py module
+sensing_dir = '../telos_spectrum_monitor/'  # Has to relative to the the telos.py module
+
 
 class sensing(threading.Thread):
     '''
     classdocs
     '''
 
-    cmd_make = 'make telosb  install bsl,' # telos_dev missing
+    cmd_make = 'make telosb  install bsl,'  # telos_dev missing
     cmd_make_clean = 'make clean'
-    cmd_printf = ['java', '-cp', '.:./tinyos.jar', 'net.tinyos.tools.PrintfClient', '-comm', 'sf@localhost:{0}'] #'serial@{0}:115200']
-    cmd_sf = [''.join([os.environ['TOSDIR'], '/../support/sdk/c/sf/sf'])] # port, dev, speed (115200)
+    cmd_printf = ['java', '-cp', '.:./tinyos.jar', 'net.tinyos.tools.PrintfClient', '-comm', 'sf@localhost:{0}']  # 'serial@{0}:115200']
+    cmd_sf = [''.join([os.environ['TOSDIR'], '/../support/sdk/c/sf/sf'])]  # port, dev, speed (115200)
     log_txt = '''#Frequencies (in MHz): {0}
 # Data format: one line per sweep (scanning all frequencies in the order above), every entry represents
 #              the average RF power (in dBm) measured on a frequency over a period of 192 microseconds.
 
 '''
     sema_install = threading.Semaphore()
+
     def __init__(self,
-                 name = 'ttyUSB0',
-                 telos_dev = '/dev/ttyUSB0',
-                 fileName = 'data'):
-        super(sensing, self).__init__(name = ' '.join(['Telos',name]))
+            name='ttyUSB0',
+            telos_dev='/dev/ttyUSB0',
+            fileName='data'):
+        super(sensing, self).__init__(name=' '.join(['Telos', name]))
         self.id = name
         self.telos_dev = telos_dev
         self._stop = threading.Event()
         self.fileName = fileName
-        self.log_filename = ''.join([self.fileName , '_telos_{0}.txt'.format(name)])
+        self.log_filename = ''.join([self.fileName, '_telos_{0}.txt'.format(name)])
         self.logger = logging.getLogger('sensing.telos')
-
 
     def stop(self):
         self._stop.set()
@@ -51,21 +54,21 @@ class sensing(threading.Thread):
         job_dir = tempfile.mkdtemp()
         work_dir = '%s/sensing' % job_dir
         self.logger.info("START - file {0} - device {1} - work dir {2}".format(
-                        self.log_filename, self.telos_dev, work_dir))
-        shutil.copytree('/'.join([os.path.dirname(__file__),sensing_dir]), work_dir,
-                         ignore=shutil.ignore_patterns('*.pyc', 'tmp*', '.*', 'build', 'pp'))
+            self.log_filename, self.telos_dev, work_dir))
+        shutil.copytree('/'.join([os.path.dirname(__file__), sensing_dir]), work_dir,
+            ignore=shutil.ignore_patterns('*.pyc', 'tmp*', '.*', 'build', 'pp'))
         #2. make clean
         self.logger.debug(self.cmd_make_clean)
         proc = subprocess.Popen(self.cmd_make_clean,
-                                           stdout=subprocess.PIPE,
-                                           stderr=subprocess.PIPE,
-                                           shell=True,
-                                           cwd=work_dir)
-        self.logger.log(logging.DEBUG-5,"{0} - {1}".format(self.id, proc.stdout.readline().rstrip()))
-        self.logger.log(logging.DEBUG-5,"{0} - {1}".format(self.id, proc.stderr.readline().rstrip()))
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            cwd=work_dir)
+        self.logger.log(logging.DEBUG-5, "{0} - {1}".format(self.id, proc.stdout.readline().rstrip()))
+        self.logger.log(logging.DEBUG-5, "{0} - {1}".format(self.id, proc.stderr.readline().rstrip()))
         proc.wait()
         #3. make install
-        freq = range(2400,2481,2)
+        freq = range(2400, 2481, 2)
         freq = ','.join(["%s" % el for el in freq])
         freq_vec = 'FREQUENCY_VECTOR={%s} ' % freq
         #self.cmd_make = self.cmd_make.format(self.telos_dev)
@@ -74,14 +77,14 @@ class sensing(threading.Thread):
         proc_done = False
         while not proc_done:
             proc = subprocess.Popen(cmd_make,
-                                           stdout=subprocess.PIPE,
-                                           stderr=subprocess.PIPE,
-                                           shell=True,
-                                           cwd=work_dir)
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+                cwd=work_dir)
             while True:
-                self.logger.log(logging.DEBUG-5,"{0} - {1}".format(self.id, proc.stdout.readline().rstrip()))
-                self.logger.log(logging.DEBUG-5,"{0} - {1}".format(self.id, proc.stderr.readline().rstrip()))
-                if proc.poll() != None:
+                self.logger.log(logging.DEBUG-5, "{0} - {1}".format(self.id, proc.stdout.readline().rstrip()))
+                self.logger.log(logging.DEBUG-5, "{0} - {1}".format(self.id, proc.stderr.readline().rstrip()))
+                if proc.poll() is not None:
                     if proc.poll() == 0:
                         proc_done = True
                     break
@@ -91,9 +94,9 @@ class sensing(threading.Thread):
         # Prepare log file
         log_file = open(self.log_filename, 'w')
         log_file.write(self.log_txt.format(freq))
-        log_file.seek(-1,2)
+        log_file.seek(-1, 2)
         #4. Run serial forwarder and printf
-        port = str(int(self.telos_dev.replace('/dev/ttyUSB','')) + 9002)
+        port = str(int(self.telos_dev.replace('/dev/ttyUSB', '')) + 9002)
         cmd_sf = self.cmd_sf[:]
         cmd_sf.append(port)
         cmd_sf.append(self.telos_dev)
@@ -106,15 +109,15 @@ class sensing(threading.Thread):
         self.logger.debug(' '.join(cmd_printf))
 
         proc = subprocess.Popen(cmd_sf,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                cwd=work_dir)
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=work_dir)
         proc_print = subprocess.Popen(cmd_printf,
-                                stdout=log_file,
-                                stderr=subprocess.PIPE,
-                                cwd=work_dir)
+            stdout=log_file,
+            stderr=subprocess.PIPE,
+            cwd=work_dir)
         self.logger.info("RUNNING - file {0} - device {1} - work dir {2}".format(
-                        self.log_filename, self.telos_dev, work_dir))
+            self.log_filename, self.telos_dev, work_dir))
         self.sema_install.release()
         self._stop.wait()
         proc.kill()
@@ -123,7 +126,8 @@ class sensing(threading.Thread):
         log_file.close()
         shutil.rmtree(job_dir)
         self.logger.info("STOP - file {0} - device {1} - work dir {2}".format(
-                        self.log_filename, self.telos_dev, work_dir))
+            self.log_filename, self.telos_dev, work_dir))
+
 
 def list_devs():
     cmd_grep_nodes = "motelist | grep /dev/ttyUSB"
@@ -136,6 +140,7 @@ def list_devs():
             break
         dev_list.append((x[0], x[1]))
     return dev_list
+
 
 def main():
     import plot
@@ -151,7 +156,7 @@ def main():
     telos_thr = []
     plot_thr = []
     for x in list_devs():
-        telos_thr.append(sensing(name = x[0], telos_dev = x[1], fileName = 'data'))
+        telos_thr.append(sensing(name=x[0], telos_dev=x[1], fileName='data'))
         telos_thr[-1].start()
     if not telos_thr:
         logger.warning('No devices found, exiting...')
@@ -159,7 +164,7 @@ def main():
     sensing.sema_install.acquire()
     logger.debug("All telos started...")
     for x in list_devs():
-        plot_thr.append(plot.lplot(name = 'Telos {0}'.format(x[0]), fileName= 'data_telos_{0}.txt'.format(x[0])))
+        plot_thr.append(plot.lplot(name='Telos {0}'.format(x[0]), fileName='data_telos_{0}.txt'.format(x[0])))
         plot_thr[-1].start()
     sensing.sema_install.release()
     while True:
